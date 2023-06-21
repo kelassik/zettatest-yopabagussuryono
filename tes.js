309ed90d953b8ee836196ea766f467d8d20a4f11
kelassik <101212480+kelassik@users.noreply.github.com>
2023-06-21 11:44:42 +0700

    tes.js

--- tes.js
+++ tes.js
@@ -3,14 +3,14 @@
 const input = [8, 7, 7, 9, 5, 4, 2, 9];
 
 function result(input) {
-  // Your Code Here
-  let len = input.length; // calculate total array
-  let mid = Math.floor(len/2) //
-  if (len % 2 != 0){
-    return input[mid];     
-  } else {
-    return (input[mid-1] + input[mid]) / 2.0
-  }
+
+  const sum_v1 = input.reduce((acc, number) => acc + number, 0);
+
+  const median = sum_v1 / input.length;
+
+  const pembulatan = Math.ceil(median);
+  // floor pembulatan kebawah, ceil pembulatan keatas
+  return pembulatan;
 
 }
 
